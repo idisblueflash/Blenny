8c22b2d9e2821612817329fac4bb099c37646232
Hu Songtao <idisblueflash@gmail.com>
2011-03-11 11:13:07 +0800

    屏蔽老功能

--- mod/poster.pl
+++ mod/poster.pl
@@ -1,75 +1,75 @@
 #!/usr/bin/perl 
-use lib 'mod/' ;
+use lib 'mod/';
 use strict;
 use warnings;
 use Xgoogle;
-use Url2title;
 
-my($user,$userhost,$chan,$msg) = @ARGV;
+# use Url2title;
+
+my ( $user, $userhost, $chan, $msg ) = @ARGV;
+
 #print "user: $user, userhost: $userhost, chan: $chan, msg: $msg\n";
 
-
-
 my @result;
-    #keyword mod 移除 关键字 模块
 
-    #xgoogle mod
-    my $re = &Xgoogle::main($msg); 
-    if ($re) {
-        my @returns= split (/\0/,$re);
-        foreach(@returns){
-            push (@result,"$chan,,$user,,$_");
-        }
+#keyword mod 移除 关键字 模块
+
+#xgoogle mod
+my $re = &Xgoogle::main($msg);
+if ($re) {
+    my @returns = split( /\0/, $re );
+    foreach (@returns) {
+        push( @result, "$chan,,$user,,$_" );
     }
+}
+if (0) { #将旧功能屏蔽，参考用。
 
     #url to tilte mod
     my $re = &Url2title::main($msg);
-    if ($re) {push (@result,"$chan,,$user,,$re");}
-    
-$msg = &ascii_save_char($msg);
-#mods to plug in
+    if ($re) { push( @result, "$chan,,$user,,$re" ); }
+
+    $msg = &ascii_save_char($msg);
+
+    #mods to plug in
     #show emote
     my $re = qx{perl mod/emote.pl "$msg" "$user"};
-    if ($re) {push (@result,"$chan,,$user,,$re");}
+    if ($re) { push( @result, "$chan,,$user,,$re" ); }
 
     #get currency
     my $re = qx{perl mod/currency.pl "$msg"};
-    if ($re) {push (@result,"$chan,,$user,,$re");}
-
+    if ($re) { push( @result, "$chan,,$user,,$re" ); }
 
     #sotre in mysql
     my $re = qx{perl mod/store_in_mysql.pl "$user" "$userhost" "$chan" "$msg"};
-    if ($re) {push (@result,"$chan,,$user,,$re");}
+    if ($re) { push( @result, "$chan,,$user,,$re" ); }
 
     #ip to geo_location
     my $re = qx{perl mod/ip.pl "$userhost" "$msg"};
-    if ($re) {push (@result,"$chan,,$user,,$re");}
-    
+    if ($re) { push( @result, "$chan,,$user,,$re" ); }
+
     #todo mod
     my $re = qx{perl mod/todo.pl "$msg"};
-    if ($re) {push (@result,"$chan,,$user,,$re");}
-
-print map {"$_\n"} @result;
-
-
+    if ($re) { push( @result, "$chan,,$user,,$re" ); }
+}
+print map { "$_\n" } @result;
 
 sub ascii_save_char() {
     my $msg = shift;
     if ( $msg =~ m/\0/ ) {
-         $msg =~ s/\0//g ;
+        $msg =~ s/\0//g;
     }
 
     if ( $msg =~ m/\$/ ) {
-        $msg =~ s/\$/\\\$/g ;
+        $msg =~ s/\$/\\\$/g;
     }
     elsif ( $msg =~ m/\`/ ) {
-        $msg =~ s/\`/\\\`/g ;
+        $msg =~ s/\`/\\\`/g;
     }
     elsif ( $msg =~ m/\"/ ) {
-         $msg =~ s/\"/\\\"/g ;
+        $msg =~ s/\"/\\\"/g;
     }
     elsif ( $msg =~ m/\\/ ) {
-        $msg =~ s/\\/\\\\/g ;
+        $msg =~ s/\\/\\\\/g;
     }
     return $msg;
 }
